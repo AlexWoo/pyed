ebf8849634b2501220e2c9c283134b5a5f9a9e5a
武杰 <wj19840501@gmail.com>
2016-03-30 15:24:19 +0800

    master sig process optimize

--- pyevent/tcpclient.py
+++ pyevent/tcpclient.py
@@ -3,10 +3,12 @@ from tcpconnection import tcpconnection
 
 
 class tcpclient(object):
-    def __init__(self, cliconf):
+    def __init__(self, cliconf, evs, tms):
         self._peerhost = cliconf["host"]
         self._peerport = cliconf["port"]
         self._cliconf = cliconf
+        self._evs = evs
+        self._tms = tms
 
     def connect(self):
         try:
@@ -19,7 +21,7 @@ class tcpclient(object):
         try:
             self._sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
             self._sock.connect((addr[0], addr[1]))
-            c = tcpconnection(self._sock, self._cliconf)
+            c = tcpconnection(self._sock, self._cliconf, self._evs, self._tms)
             return c
         except socket.error, e:
             sys.stdout.write("Error occur connect to TCPServer(%s: %d): %s\n" % (addr[0], addr[1], e))
