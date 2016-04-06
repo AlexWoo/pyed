6c0be71270f014afdf393a9ee3c075c82962b140
武杰 <wj19840501@gmail.com>
2016-04-06 11:12:09 +0800

    add slp scheduler

---
+++ pyworker/slpscheduler.py
@@ -0,0 +1,14 @@
+class slpschedmanager(object):
+    def __init__(self, pesys):
+        self._log = pesys.log
+        self._loader = pesys.loader
+    
+    def create(self, schedtype):
+        pass
+
+class slpscheduler(object):
+    def query(self, *args):
+        pass
+
+    def callslp(self, *args):
+        pass
